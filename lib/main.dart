90953c5579d5bd875c6b36c23b27cd37fedf9de9
zzb <>
2019-10-09 17:44:06 +0800

    image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,21 +10,9 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      onGenerateRoute: (RouteSettings settings) {
-        print("routeName");
-        return MaterialPageRoute(builder: (context) {
-          String routeName = settings.name;
-          // 如果访问的路由页需要登录，但当前未登录，则直接返回登录页路由，
-          // 引导用户登录；其它情况则正常打开路由。
-        });
-      },
       //注册路由表
       routes: {
-        "tip2": (context) {
-          return TipRoute(text: ModalRoute.of(context).settings.arguments);
-        },
         "/": (context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
-        "echo": (context) => EchoRoute()
       },
     );
   }
@@ -65,26 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
-            FlatButton(
-              child: Text("open new route"),
-              textColor: Colors.blue,
-              onPressed: () {
-                // 导航到新路由
-                Navigator.of(context).pushNamed("echo", arguments: "hi");
-
-                // Navigator.pushNamed(context, "new_page");
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //       builder: (context) {
-                //         return RouterTestRoute();
-                //       },
-                //       fullscreenDialog: false,
-                //       maintainState: false,
-                //     ));
-              },
-            ),
-            RandomWordsWidget()
+            Image.asset('assets/images/a1.png'),
           ],
         ),
       ),
@@ -93,86 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class TipRoute extends StatelessWidget {
-  TipRoute({
-    Key key,
-    @required this.text, // 接收一个text参数
-  }) : super(key: key);
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("提示"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(18),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text(text),
-              RaisedButton(
-                onPressed: () => Navigator.pop(context, "我是返回值"),
-                child: Text("返回"),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RouterTestRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () async {
-          // 打开`TipRoute`，并等待返回结果
-          var result = await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) {
-                return TipRoute(
-                  // 路由参数
-                  text: "我是提示xxxx",
-                );
-              },
-            ),
-          );
-          //输出`TipRoute`路由返回结果
-          print("路由返回值: $result");
-        },
-        child: Text("打开提示页"),
-      ),
-    );
-  }
-}
-
-class EchoRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    //获取路由参数
-    var args = ModalRoute.of(context).settings.arguments;
-    //...省略无关代码
-    return Text(args);
-  }
-}
-
-class RandomWordsWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-   // 生成随机字符串
-    final wordPair = new WordPair.random();
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: new Text(wordPair.toString()),
     );
   }
 }
